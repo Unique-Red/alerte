6210963e55f613be2b7025f1741df5279163a8f7
Solomon Great <greatsoma2019@gmail.com>
2024-09-27 03:33:17 +0100

    started mobile responsiveness

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const Aside = document.querySelector('.aside');
+const sideNav = document.querySelector('.nav-items');
 const asideToggler = document.querySelectorAll('.nav-link.aside-toggler');
+const mainToggler = document.querySelector('.toggler button.toggler-btn');
 
 console.log(asideToggler)
 asideToggler.forEach((toggler)=>{
@@ -8,4 +10,23 @@ asideToggler.forEach((toggler)=>{
         console.log("toggled")
     })
    
+})
+
+window.addEventListener('load',(e)=>{
+    window.addEventListener('resize',()=>{
+        if(window.innerWidth > 768){
+            sideNav.classList.remove('hidden')
+        }
+        else{
+            sideNav.classList.add('hidden')
+        }
+    })
+    console.log(e)
+    if(window.innerWidth < 768){
+        sideNav.classList.add('hidden')
+    }
+})
+
+mainToggler.addEventListener('click',()=>{
+    sideNav.classList.toggle('hidden')
 })
